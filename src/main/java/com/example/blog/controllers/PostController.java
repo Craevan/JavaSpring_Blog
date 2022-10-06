7170415bddd749b4de3019da112fa5da05299f4d
Crevan <a.siverskii@gmail.com>
2022-10-06 18:33:35 +0300

    [update] interface injection; add @Qualifier for Database

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.example.blog.controllers;
 
-import com.example.blog.services.impl.InMemoryPostService;
+import com.example.blog.services.api.PostService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PostController {
 
-    private final InMemoryPostService inMemoryPostService;
+    private final PostService postService;
 
-    public PostController(InMemoryPostService inMemoryPostService) {
-        this.inMemoryPostService = inMemoryPostService;
+    public PostController(@Qualifier("dbPostService") PostService postService) {
+        this.postService = postService;
     }
 
     @GetMapping("/")
     public String hello(Model model, @RequestParam(required = false) String query) {
-        model.addAttribute("posts", inMemoryPostService.search(query));
+        model.addAttribute("posts", postService.search(query));
         return "hello";
     }
 }
